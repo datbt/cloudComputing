709af705aa52bb7277d12b3928d8fc571862c5e6
BuiTienDat <datxuhp94@gmail.com>
2019-11-28 21:39:37 +0700

    up

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ $user = "xftogjfqyxqtey"; /* User */
 $password = "38b32364fdbc8622f2ae5f58b33ecd202869267a01999706ab73414e327263c4"; /* Password */
 $dbname = "d2gmj18ps8f95a"; /* Database name */
 $port = '5432';
-$con = pg_connect("host=$host port=<port> dbname=$dbname user=$user password=$password");
+$con = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 // Check connection
 if (!$con) {
     echo "An error occurred.\n";
